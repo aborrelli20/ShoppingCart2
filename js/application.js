863b5e5135ef543c48bd5b86b66dfeedfe259bb0
aborrelli20 <71987074+aborrelli20@users.noreply.github.com>
2022-01-29 11:31:24 -0500

    added subtotal, new item, and remove functionality

---
+++ js/application.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+
+  window.addEventListener('keyup', function (event) {
+    $('tbody tr').each(function (i, ele) {
+      var price = parseInt($(ele).find('.price').text());
+      var number = $(ele).find('.number').children('input').val();
+      var subtotal = price * number;
+      $(ele).children('.subtotal').html(subtotal);
+      return subtotal;
+    });
+  });
+
+  $('#addItem').on('submit', function (event) {
+    event.preventDefault();
+    var item = $(this).find('[name=item]').val();
+    var price = $(this).find('[name=price]').val();
+
+    $('tbody').children().last().prev().prev().after('<tr>' + '<td class="item">' + item + '</td>' +
+      '<td class="price">' + price + '</td>' + '<td></td>' + '<td class="number"><label for="quantity">QTY</label><input type="text"></td>' +
+      '<td><button class="button remove">Remove</button></td>' +
+      '<td class="subtotal">0</td>' + '</tr>');
+  });
+
+  $(document).on('click', '.remove', function (event) {
+    $(this).closest('tr').remove();
+  });
+
+});
